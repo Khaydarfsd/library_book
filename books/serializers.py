426391ce3dea0536ebb31e14037c9994f966f105
khaydarfsd <haydartursunov05@gmail.com>
2023-04-04 20:38:06 +0500

    library project

---
+++ books/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'content', 'subtitle', 'author', 'isbn', 'price',)
+
+    def validate(self, data):
+        title = data.get('title', None)
+        author = data.get('author', None)
+
+        #check title if it containes only alphabetical chars
+        if not title.isalpha():
+            raise ValidationError(
+                {
+                    'status': False,
+                    'message': 'Kitobni sarlavhasi harflardan tashkil topishi kk!'
+                }
+            )
+        #check author and title from database existence
+        if Book.objects.filter(title=title, author=author).exists():
+            raise ValidationError(
+                {
+                    'status': False,
+                    'message': 'Kitob sarlavhasi va muallifi bir xil bo\'lgan kitobni yuklay olmaysiz!'
+                }
+            )
+
+        return data
+
+    def validate_price(self, price):
+        if price < 0 or price > 999999999:
+            raise ValidationError(
+                {
+                    'status': False,
+                    'message': 'Narx notogri kiritilgan!'
+                }
+            )
